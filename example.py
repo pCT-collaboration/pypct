3c857e4dabea28088e7cd99466f41d038fe53404
ford <ford@a3e4e0d1-83fe-448f-9238-8876f0361cc7>
2013-02-26 23:46:31 +0000

    First commit.

---
+++ example.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+
+import numpy as np
+import matplotlib.pyplot as plot
+
+import pypct.roi as pctroi
+import pypct.image as pctimage
+
+import os
+
+nameformat = 'example/lucy_scan4_newcal_2012-05-25b_i{iteration:02d}_s{slicenum:02d}.txt'
+
+iterations = [8]
+slicenums = [10]
+
+currentpath = os.path.dirname(__file__)
+print currentpath
+
+images = []
+for iteration in iterations:
+	image = pctimage.Image(iteration)
+	for slicenum in slicenums:
+		filename = nameformat.format(iteration=iteration, slicenum=slicenum)
+		filename = os.path.join(currentpath, filename)
+		print filename
+		image.addSlice(filename, slicenum)
+	images.append(image)
+print
+
+imageshape = None
+for image in images:
+	if imageshape != None and imageshape != image.shape:
+		raise Exception("Dimensions of images do not match!")
+	imageshape = image.shape
+
+regions = []
+regions.append(pctroi.RectangleROI('poly1', (103, 51), (51, 55), imageshape))
+regions.append(pctroi.RectangleROI('poly2', (103, 148), (51, 55), imageshape))
+regions.append(pctroi.CircleROI('air', (80, 79), 4, imageshape))
+regions.append(pctroi.CircleROI('bone', (176, 175), 4, imageshape))
+
+images[0].plotSlice(slicenums[0], regions).savefig(os.path.join(currentpath, 'example', 'regions.png'))
+
+for region in regions:
+	print region.name
+	print '%4s %5s %6s %6s' % ('Iter', 'Slice', 'Mean', 'RMS')
+	for image in images:
+		for slice in image.slices:
+			mean, sigma = region.measure(slice.pixels)
+			print '%4d %5d %6f %6f' % (image.iteration, slice.num, mean, sigma)
+		print
+	
+line = pctroi.RectangleROI('line', (0, 77), (256, 3), imageshape)
+images[0].plotSlice(slicenums[0], line).savefig(os.path.join(currentpath, 'example', line.name + '.png'))
+line.plotLineProfile(images[0].getSlice(slicenums[0]).pixels, axis=0).savefig(os.path.join(currentpath, 'example', line.name + '-profile.png'))
+
+circle = pctroi.CircleROI('circle', (80, 79), 15, imageshape)
+images[0].plotSlice(slicenums[0], circle).savefig(os.path.join(currentpath, 'example', circle.name + '.png'))
+circle.plotRadialProfile(images[0].getSlice(slicenums[0]).pixels).savefig(os.path.join(currentpath, 'example', circle.name + '-profile.png'))
---
+++ pypct/ctconvert.py
@@ -0,0 +1,63 @@
+# Tools for converting x-ray CT DICOM files to RSP
+
+# TODO: return a pypct Image object, with sorted slices (need to look at slice number in DICOM file)
+
+import dicom
+from numpy import *
+import image
+
+hu_conv = ((0.0, 0.0),
+           (800.0, 0.8),
+           (900.0, 0.95),
+           (1000.0, 1.0),
+           (1050.0, 1.05),
+           (4095.0, 2.45))
+
+def convertToRSP(hu_value):
+    for i in range(len(hu_conv)-1):
+        if hu_value >= hu_conv[i][0] and hu_value < hu_conv[i+1][0]:
+            slope = float((hu_conv[i+1][1] - hu_conv[i][1]) / (hu_conv[i+1][0] - hu_conv[i][0]))
+            rsp = slope * (hu_value - hu_conv[i][0]) + hu_conv[i][1]
+            return rsp
+    return 0
+
+def processDicomFile(filename, create_hu_file=False, create_rsp_file=True):
+    '''Open the named dicom file, and create text files containing pixel data. 
+    Returns an array containing RSP values.
+    '''
+    print 'Processing', filename
+    
+    slice = dicom.read_file(filename)
+    im = slice.pixel_array
+    slice_num = slice.InstanceNumber
+
+    rsp_data = zeros((im.shape[0], im.shape[1]))
+
+    if create_hu_file: outfile_hu = open('hu-s%d.txt' % slice_num, 'w')
+    if create_rsp_file: outfile_rsp = open('rsp-s%d.txt' % slice_num, 'w')
+    
+    for i in range(im.shape[0]):
+        for j in range(im.shape[1]):
+            if create_hu_file: outfile_hu.write('%f ' % im[i][j])
+            rsp_data[i][j] = convertToRSP(float(im[i][j]))
+            if create_rsp_file: outfile_rsp.write('%f ' % rsp_data[i][j])
+        if create_hu_file: outfile_hu.write('\n')
+        if create_rsp_file: outfile_rsp.write('\n')
+    
+    if create_hu_file: outfile_hu.close()
+    if create_rsp_file: outfile_rsp.close()
+
+    return rsp_data
+
+def processDicomDirectory(dirname, extension='.dcm'):
+	'''Open all files in dirname with extension.
+	Returns a 3D array of RSP values. Indices are: [x (horiz)][y (vert)][z (slice)]
+	'''
+	stack = []
+	filelist = os.listdir(dirname)
+	for filename in filelist:
+		if filename[-4:] == extension:
+			stack.append(processDicomFile(dirname + '/' + filename))
+	stack = dstack(stack)
+	return stack
+	
